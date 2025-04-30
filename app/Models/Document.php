bee86036465870a0e8a5c95944ae7b3d295dea23
Shaun Menezes <shaun.menezes1724@gmail.com>
2025-04-30 21:14:38 +0530

    scout integration

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Document extends Model
 {
+    use Searchable;
     protected $fillable = [
         'title',
         'content',
