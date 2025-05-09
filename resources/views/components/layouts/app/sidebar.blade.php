37dfe19996b7759cfb6cf2290f589444aff3a83e
Shaun Menezes <shaun.menezes1724@gmail.com>
2025-05-09 20:02:43 +0530

    navbar items

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,6 +14,8 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="" :href="route('chat.page')" :current="request()->routeIs('chat.page')" wire:navigate>{{ __('Chat') }}</flux:navlist.item>
+                    <flux:navlist.item icon="" :href="route('document.add.page')" :current="request()->routeIs('document.add.page')" wire:navigate>{{ __('Document') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
